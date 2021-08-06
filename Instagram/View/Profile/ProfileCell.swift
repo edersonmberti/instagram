42a7951dae6f3b6c28bc4297b73d147e8708e12c
Ederson Berti <edersonmberti@gmail.com>
2021-08-06 20:30:26 -0300

    feat: add profile screen layout

--- Instagram/View/Profile/ProfileCell.swift
+++ Instagram/View/Profile/ProfileCell.swift
@@ -11,12 +11,23 @@ class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .lightGray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
