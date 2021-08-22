46d4936d9c3b488bf61b051b74360fbdcd1e128b
Ederson Berti <edersonmberti@gmail.com>
2021-08-22 20:25:27 -0300

    feat: add search users on search screen

--- Instagram/View/UserCell.swift
+++ Instagram/View/UserCell.swift
@@ -11,10 +11,9 @@ class UserCell: UITableViewCell {
  
     // MARK: - Properties
     
-    var user: User? {
+    var viewModel: UserCellViewModel? {
         didSet {
-            usernameLabel.text = user?.username
-            fullnameLabel.text = user?.fullname
+            configure()
         }
     }
     
@@ -61,5 +60,15 @@ class UserCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    private func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        usernameLabel.text = viewModel.username
+        fullnameLabel.text = viewModel.fullname
     }
 }
