d65a0949570fd814472d05ba59f0b3e05e77c31c
Ederson Berti <edersonmberti@gmail.com>
2021-08-30 23:34:34 -0300

    feat: order posts by timestamp on feed

--- Instagram/Controller/MainTabController.swift
+++ Instagram/Controller/MainTabController.swift
@@ -151,5 +151,10 @@ extension MainTabController: UpdatePostControllerProtocol {
     func controllerDidFinishUploadingPost(_ controller: UploadPostController) {
         selectedIndex = 0
         self.dismiss(animated: true, completion: nil)
+        
+        guard let feedNav = viewControllers?.first as? UINavigationController else { return }
+        guard let feed = feedNav.viewControllers.first as? FeedController else { return }
+        
+        feed.handleRefresh()
     }
 }
